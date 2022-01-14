38aa7e3798ed6f461bdae19f75100acdbd7945ed
Brett Adams <brett.whynot@gmail.com>
2022-01-14 12:39:09 +1000

    admin save

--- bin/badrcm.py
+++ bin/badrcm.py
@@ -222,27 +222,24 @@ class req(PersistentServerConnectionApplication):
 
             # Config
             try:
-                resp = {}
-                resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{user_context}/{APP_NAME}/configs/conf-{APP_NAME}", sessionKey=self.AUTHTOKEN, postargs={'name': form['server']}, raiseAllErrors=True)
+                resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{user_context}/{APP_NAME}/configs/conf-{APP_NAME}", sessionKey=self.AUTHTOKEN, postargs={'name': form['server']})
+                if resp.status not in [200,201,409]:
+                    return self.errorhandle(f"Adding new server '{form['server']}' failed", e, resp.status)
             except Exception as e:
-                status = resp.get('status',400)
-                if status != 409:
-                    return self.errorhandle(f"Adding new server '{form['server']}' failed", e, status)    
+                return self.errorhandle(f"Adding new server '{form['server']}' failed", e)
+                        
             
             # Password Storage
             try:
-                resp = {}
-                resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{user_context}/{APP_NAME}/storage/passwords", sessionKey=self.AUTHTOKEN, postargs={'realm': APP_NAME, 'name': form['server'], 'password': form['token']}, raiseAllErrors=True)
+                resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{user_context}/{APP_NAME}/storage/passwords", sessionKey=self.AUTHTOKEN, postargs={'realm': APP_NAME, 'name': form['server'], 'password': form['token']})
+                if resp.status not in [200,201,409]:
+                    return self.errorhandle(f"Adding token for server '{form['server']}' failed", e, status) 
+                if resp.status == 409:
+                    resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{self.USER}/{APP_NAME}/storage/passwords/{APP_NAME}%3A{server}%3A?output_mode=json&count=1", sessionKey=self.AUTHTOKEN, postargs={'password': form['token']})
+                    if resp.status not in [200,201]:
+                        return self.errorhandle(f"Updating token for server '{form['server']}' failed", e, status) 
             except Exception as e:
-                status = resp.get('status',400)
-                if status != 409:
-                    return self.errorhandle(f"Adding token for server '{form['server']}' failed", e, status)  
-                else:
-                    try:
-                        resp, _ = simpleRequest(f"{self.LOCAL_URI}/servicesNS/{self.USER}/{APP_NAME}/storage/passwords/{APP_NAME}%3A{server}%3A?output_mode=json&count=1", sessionKey=self.AUTHTOKEN, postargs={'password': form['token']}, raiseAllErrors=True)
-                    except Exception as e:
-                        status = resp.get('status',400)
-                        return self.errorhandle(f"Updating token for server '{form['server']}' failed", e, status)
+                return self.errorhandle(f"Adding token for server '{form['server']}' failed", e, status)  
             
             # Password ACL
             try:
