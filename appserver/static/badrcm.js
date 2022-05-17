4ef7b6cda1d25340fa677768717574b2053a8b7e
Brett Adams <brett.whynot@gmail.com>
2022-05-17 19:51:09 +1000

    admin save

--- appserver/static/badrcm.js
+++ appserver/static/badrcm.js
@@ -549,7 +549,7 @@ const vue = new Vue(
             return zip.generateAsync({type:"blob"}).then((content)=>{
                 this.$refs.export.close()
                 this.conf_export_loading = false
-                this.conf_export_col = false
+                this.conf_export_col = null
                 window.location.assign(window.URL.createObjectURL(content));
                 console.timeEnd(`ConfDownloadFiles ${c.server}`)
             });
