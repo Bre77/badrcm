8745a9a471bb99e5764f501ef9ba192d1d8f0e14
Brett Adams <brett.whynot@gmail.com>
2022-05-18 11:45:01 +1000

    admin save

--- appserver/static/badrcm.js
+++ appserver/static/badrcm.js
@@ -92,6 +92,7 @@ const vue = new Vue(
         conf_remove_loading: false,
         conf_export_col: null,
         conf_export_loading: false,
+        conf_export_files: {},
         idxc_columns: Array.from({length:COLUMNS_MAX}, u => ({
             server: '',
             loading: 0,
