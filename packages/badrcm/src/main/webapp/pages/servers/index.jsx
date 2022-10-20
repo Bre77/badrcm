d2e15083abae398c5ad6e66fb7db5fcbe9ed3cd9
Brett Adams <brett.whynot@gmail.com>
2022-10-21 09:37:46 +1000

    Remove defaults

--- packages/badrcm/src/main/webapp/pages/servers/index.jsx
+++ packages/badrcm/src/main/webapp/pages/servers/index.jsx
@@ -18,7 +18,7 @@ import ControlGroup from '@splunk/react-ui/ControlGroup';
 const Servers = () => {
 
     const default_name = ""
-    const default_token = "eyJraWQiOiJzcGx1bmsuc2VjcmV0IiwiYWxnIjoiSFM1MTIiLCJ2ZXIiOiJ2MiIsInR0eXAiOiJzdGF0aWMifQ.eyJpc3MiOiJicmV0dF9hZGFtcyBmcm9tIHNoLWktMGQyMjAyYzJhYjYzNTRiMmYiLCJzdWIiOiJicmV0dF9hZGFtcyIsImF1ZCI6IkJBRFJDTSIsImlkcCI6IlNwbHVuayIsImp0aSI6ImJiMWE4M2QyNGZiMDZjZTY1YjBiY2NjMzE3MzdkOTQ5YTkwYWRkMzg4ZGU1YWIyNmEyZjQxYWJkM2IwYmQ3NWUiLCJpYXQiOjE2NjU0NTIwNjAsImV4cCI6MTY3MzIyODA2MCwibmJyIjoxNjY1NDUyMDYwfQ.59l5P18hB7X7BDf9TVgtnKkA6PpGveTizaiaxPhNYHgOeTaE3fCzUyjIl1w9FzC_UoDuEcLkuT1UovnHShfK8g"
+    const default_token = ""
     const default_share = true
 
 
