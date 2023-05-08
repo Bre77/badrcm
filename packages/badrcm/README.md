1ce1950a94f808a03d579da4b421b0bcd761aa9d
Brett Adams <brett.whynot@gmail.com>
2023-05-08 10:26:05 +1000

    Refactor for dev container

--- packages/badrcm/README.md
+++
@@ -1,3 +0,0 @@
-# Badrcm
-
-Add all information required to get started with @splunk/badrcm here.
--- runfix.sh
+++
@@ -1,3 +0,0 @@
-export PATH=$(echo $PATH | sed -s 's+/opt/splunk-dev/bin:++')
-node -v
-npm run start
